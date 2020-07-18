bf74ff2bc99930b3af4da819ff7db20cbef0a989
Daniela94 <johndoe@example.com>
2020-07-18 11:10:50 -0500

    React desde cero

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import "./styles/styles.scss"
+import Card from './Card'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+    <div className="main-banner img-container l-section" id="main-banner">
+      <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+          <img className="main-banner__img" src="https://www.chevrolet.com.pa/content/dam/chevrolet/na/mx/es/index/cars/2019-aveo/aveo-shared-assets/2019-aveo-masthead-imwidth=1200.jpg" alt="banner" />
+          <div className="main-banner__data s-center">
+            <p className="t2 s-mb-0">Aveo 2018.1</p>
+            <p> Gris Plata</p>
+            <a href="https://www.chevrolet.com.pa/autos/aveo" className="button">Comprar</a>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
+    <div className="ed-grid m-grid-3">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  </>
+)
 
 export default App;
