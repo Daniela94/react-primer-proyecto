6a999d910a18c79f3df406d7a92c3e58c32a9d0c
Daniela <04elarodriguez@gmail.com>
2020-07-19 15:04:51 -0500

    componentes clase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 // import './App.css';
 import "./styles/styles.scss"
 import Card from './Card'
+import Banner from './Banner'
+import Formulario from './Formulario'
 
 const cursos = [
   {
@@ -37,30 +39,8 @@ const cursos = [
 
 const App = () => (
   <>
-    <div className="main-banner img-container l-section" id="main-banner">
-      <div className="ed-grid lg-grid-6">
-        <div className="lg-cols-4 lg-x-2">
-          <img className="main-banner__img" src="https://www.chevrolet.com.pa/content/dam/chevrolet/na/mx/es/index/cars/2019-aveo/aveo-shared-assets/2019-aveo-masthead-imwidth=1200.jpg" alt="banner" />
-          <div className="main-banner__data s-center">
-            <p className="t2 s-mb-0">Aveo 2018.1</p>
-            <p> Gris Plata</p>
-            <a href="https://www.chevrolet.com.pa/autos/aveo" className="button">Comprar</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="ed-grid m-grid-3">
-      {
-        cursos.map( curso =>
-           <Card 
-            titulo={curso.titulo}
-            imagen={curso.imagen}
-            imagenProfesor={curso.imagenProfesor}
-            profesor={curso.profesor}
-            precio={curso.precio}
-           />)
-      }
-    </div>
+    <Banner />
+    <Formulario name="EDteam"/>
   </>
 )
 
---
+++ src/Formulario.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+
+class Formulario extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      nombre: "",
+      correo: "",
+      fecha: new Date()
+    }
+
+    this.setNombre = this.setNombre.bind(this)
+    this.setCorreo = this.setCorreo.bind(this)
+  }
+
+  setNombre(e) {
+    this.setState({
+      nombre: e.target.value
+    })
+  }
+
+  setCorreo(e) {
+    this.setState({
+      correo: e.target.value
+    })
+  }
+
+  actualizarFecha() {
+    this.setState({
+      fecha: new Date()
+    })
+  }
+
+  render() {
+    return (
+      <div className="ed-grid">
+        <h1>Formulario {this.props.name}</h1>
+        <h4>Fecha actual: {Math.ceil(this.state.fecha/1000)}</h4>
+        <form className="ed-container" id="form-element">
+          <div className="ed-item lg-40 form__item">
+            <label>Nombre completo</label>
+            <input type="text" 
+              onChange={this.setNombre} 
+            />
+          </div>
+          <div className="ed-item lg-40 form__item">
+            <label>Correo electrónico</label>
+            <input type="email" 
+              onChange={this.setCorreo}
+            />
+          </div>
+          <div className="ed-item small">
+            <input type="submit" className="button small" />
+          </div>
+        </form>
+        <div>
+          <h2>{`Hola ${this.state.nombre}`}</h2>
+          <p>{`Tu correo es ${this.state.correo}`}</p>
+        </div>
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    let formElement = document.getElementById('form-element')
+    // console.log(formElement)
+    this.intervaloFecha = setInterval(() => {
+      this.actualizarFecha()
+    }, 1000)
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // console.log(prevProps)
+    // console.log(prevState)
+    // console.log('--------')
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervaloFecha)
+  }
+
+}
+
+export default Formulario
